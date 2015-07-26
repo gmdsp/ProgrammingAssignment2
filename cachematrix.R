35c134eae1fa6ab4f1c0848b7fc4a97ea7c71f09
gmdsp <goranm@ieee.org>
2015-07-26 13:36:23 -0400

    Programming Assignment 2: makeCacheMatrix and cacheSolve functions updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions makeCacheMatrix and cacheSolve enable more efficient matrix inverse computations 
+## by avoiding unnecessary matrix inverse computations if matrix inverse has already been computed
+## and there is no change in the matrix whose inverse has to be computed. This can greatly improve
+## speed of programs that use loops and compute matrix inverses inside the loops.
 
-## Write a short comment describing this function
+
+
+
+## The makeCacheMatrix function takes a matrix as an argument and creates a list of functions
+## that can set the matrix, get the matrix, set the matrix's inverse and get the matrix's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<-NULL  ## Matrix inverse set to null
+    
+    set<-function(y=matrix()) {    ## set function assingn new matrix to a variable
+        x<<-y       ## assign the set function's argument (y) to the variable x from the parent environment
+        inv<<-NULL  ## matrix inverse not computed, so it is set to NULL
+    }
+    get<-function() x   ## get function reads the matrix 
+    getInv<-function() inv   ## getInv function reads the matrix inverse
+    setInv<-function(m_inv) inv<<-m_inv   ## setInv function assigns the matrix inverse to the variable inv from
+                                          ## the pharent environment
+    list(set = set,get = get, getInv=getInv, setInv=setInv)   ## makeCacheMatrix function returns the list of functions
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve function checks if matrix, formated as a list by the function makeCacheMatrix, 
+## passed as an argument has its inverse already computed. If not it computes the 
+## matrix inverse. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv<-x$getInv()   ## read matrix inverse   
+    if(!is.null(inv))   ## check if matrix inverse has already been computed
+    {
+        message("getting cached matrix inverse")
+        return(inv)     ## if the matrix inverse has been computed, read it from cache
+    }
+    else   ## if matrix inverse has not been already computed, compute it using solve() function
+    {
+        m_x<-x$get()
+        inv<-solve(m_x,...)   ## compute matrix inverse
+        x$setInv(inv)
+        return(inv)
+    }
 }
